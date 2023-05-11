701831abe82836a640b2bb7f4854eac4152f5472
Floh02 <flavioloh02@gmail.com>
2023-05-11 14:10:01 +0200

    update

--- Flavio-Loh/progetto_personale/javascript/search.js
+++ Flavio-Loh/progetto_personale/javascript/search.js
@@ -1,38 +1,29 @@
-function myFunction() {
-  const searchInput = document.getElementById("search");
-  const sections = document.querySelectorAll("section[id]");
-  searchInput.addEventListener("keyup", function(event) {
-  const query = event.target.value.toLowerCase();
-  sections.forEach(function(section) {
-    const title = section.querySelector("h3").innerText.toLowerCase();
-    if (title.indexOf(query) !== -1 || section.id.toLowerCase().indexOf(query) !== -1) {
-      section.style.display = "block";
+
+ // recupera tutti gli elementi della lista degli articoli
+const items = document.querySelectorAll('.item');
+
+// funzione per filtrare gli articoli in base alla ricerca
+function filterItems(searchTerm) {
+
+    // converte il testo di ricerca in minuscolo
+  searchTerm = searchTerm.toLowerCase();
+
+  // itera su ogni elemento e nasconde quelli che non corrispondono alla ricerca
+  items.forEach(function(item) {
+
+    const name = item.querySelector('h3').textContent.toLowerCase();
+  
+    // controlla se il nome dell'elemento inizia con la lettera o le lettere cercate
+    if (name.startsWith(searchTerm)) {
+      item.style.display = "flex";
     } else {
-      section.style.display = "none";
+      item.style.display = "none";
     }
   });
-});
 }
 
-
-
-
-
-
-
-
-
-//var input, filter, ul, li, a, i, txtValue;
-//    input = document.getElementById("myInput");
-//    filter = input.value.toUpperCase();
-//    id = document.getElementById("myUL");
-//    li = id.getElementsByTagName("li");
-//    for (i = 0; i < li.length; i++) {
-//        a = li[i].getElementsByTagName("a")[0];
-//        txtValue = a.textContent || a.innerText;
-//        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-//            li[i].style.display = "";
-//        } else {
-//            li[i].style.display = "none";   
-//        }
-//    }
+// gestisce l'evento di modifica dell'input di ricerca
+const searchInput = document.querySelector('#search');
+searchInput.addEventListener('input', function() {
+  filterItems(this.value);
+});
