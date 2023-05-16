362b5b63debc790a96340fa7c0aa5bddeb978c4a
CampanaIvan <119603399+CampanaIvan@users.noreply.github.com>
2023-05-16 20:07:18 +0200

    commit

--- campana-ivan/ProgettoPersonale/ProgettoPersonale.js
+++ campana-ivan/ProgettoPersonale/ProgettoPersonale.js
@@ -3,20 +3,6 @@
         sessionStorage.setItem('prodotti_carrello', JSON.stringify(Carrello));
     }
 
-    /*function aggiungiprodotto(frase) {
-        let carrello = document.getElementById("Carrello");
-        let riga = carrello.insertRow();
-        let cella = riga.insertCell();
-        let CarrelloSingolo = JSON.parse(sessionStorage.getItem('prodotti_carrello'));
-        CarrelloSingolo.push(frase);
-        sessionStorage.setItem('prodotti_carrello', JSON.stringify(CarrelloSingolo) );
-        cella.innerHTML = frase;
-        console.log(JSON.parse(sessionStorage.getItem('prodotti_carrello')));
-        
-    }
-    console.log(JSON.parse(sessionStorage.getItem('prodotti_carrello')));*/
-
-
     function addToCart(productName, price) {
         var cartItems = JSON.parse(localStorage.getItem('cart')) || [];
         cartItems.push({ name: productName, price: price });
