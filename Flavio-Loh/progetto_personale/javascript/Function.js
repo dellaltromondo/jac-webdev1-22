fbbdc566bf35cef73b79631ae5d62d7329343e07
Floh02 <flavioloh02@gmail.com>
2023-05-17 10:20:01 +0200

    update

--- Flavio-Loh/progetto_personale/javascript/Function.js
+++ Flavio-Loh/progetto_personale/javascript/Function.js
@@ -1,39 +1,36 @@
 window.addEventListener("load", function() {
-  // Quando la pagina è stata completamente caricata, carica il carrello dall'archiviazione di sessione
   let cart = sessionStorage.getItem("cart");
   if (cart) {
     cart = JSON.parse(cart);
   } else {
     cart = [];
   }
-  
-  // Crea una tabella per il carrello e aggiungi ogni prodotto come una riga nella tabella
   const cartTable = document.querySelector("#cart-items");
+  let total = 0;
+
   for (const item of cart) {
     const row = document.createElement("tr");
     const nameCell = document.createElement("td");
-    nameCell.textContent = item.name;   
-    const quantityCell = document.createElement("td");  
+    nameCell.textContent = item.name;
+    const quantityCell = document.createElement("td");
     quantityCell.textContent = item.quantity;
     const priceCell = document.createElement("td");
-    priceCell.textContent = "$" + item.price*item.quantity;
+    priceCell.textContent = "$" + item.price * item.quantity;
     const removeButtonCell = document.createElement("td");
     const removeButton = document.createElement("button");
     removeButton.textContent = "Rimuovi";
-    // Aggiungi un evento di click al pulsante "Rimuovi" per rimuovere il prodotto dal carrello
-    removeButton.addEventListener("click", function () {
+    removeButton.addEventListener("click", function() {
       const index = cart.indexOf(item);
       if (item.quantity > 1) {
         item.quantity--;
         quantityCell.textContent = item.quantity;
-        priceCell.textContent = "$" + item.price*item.quantity;
-        sessionStorage.setItem("cart", JSON.stringify(cart));
+        priceCell.textContent = "$" + item.price * item.quantity;
       } else {
         cart.splice(index, 1);
         row.remove();
-        priceCell.textContent = "$" + item.price;
-        sessionStorage.setItem("cart", JSON.stringify(cart));
       }
+      sessionStorage.setItem("cart", JSON.stringify(cart));
+      updateCartTotal();
     });
     removeButtonCell.appendChild(removeButton);
     row.appendChild(nameCell);
@@ -41,39 +38,52 @@ window.addEventListener("load", function() {
     row.appendChild(priceCell);
     row.appendChild(removeButtonCell);
     cartTable.appendChild(row);
+
+    total += item.price * item.quantity;
+  }
+  
+  updateCartTotal();
+  
+  function updateCartTotal() {
+    total = 0;
+    for (const item of cart) {
+      total += item.price * item.quantity;
+    }
+    const totalElement = document.querySelector("#total");
+    totalElement.innerHTML = "<h3>Totale: $" + total.toFixed(2) + "</h3>";
   }
 });
 
-// Aggiungi un evento di click a ogni pulsante "Aggiungi al carrello"
-const addToCartButton = document.querySelectorAll(".add-to-cart");
-addToCartButton.forEach(button => {
-  button.addEventListener("click", function () {
-    const name = this.dataset.name;
-    const price = this.dataset.price;
-    const quantity = this.dataset.quantity;
-    const item = { name, price, quantity };
+const addToCartButtons = document.querySelectorAll(".add-to-cart");
+addToCartButtons.forEach(button => {
+  button.addEventListener("click", function() {
+    const name = button.dataset.name;
+    const price = parseFloat(button.dataset.price);
+    const quantity = parseInt(button.dataset.quantity);
+    const item = {
+      name: name,
+      price: price,
+      quantity: quantity
+    };
     let cart = sessionStorage.getItem("cart");
-    // Se il carrello esiste già, aggiungi il prodotto al carrello, altrimenti crea un nuovo carrello
     if (cart) {
       cart = JSON.parse(cart);
     } else {
       cart = [];
     }
     let incrementaQuantita = false;
-    for (const itemCart of cart){
-      // Se il prodotto esiste già nel carrello, aumenta la quantità
-      if(itemCart.name === item.name){
+    for (const itemCart of cart) {
+      if (itemCart.name === item.name) {
         incrementaQuantita = true;
         itemCart.quantity++;
-        sessionStorage.setItem("cart", JSON.stringify(cart));
         break;
       }
     }
-    // Se il prodotto non esiste nel carrello, aggiungi il prodotto al carrello con una quantità di 1
-    if(!incrementaQuantita){
+    if (!incrementaQuantita) {
       item.quantity = 1;
       cart.push(item);
-      sessionStorage.setItem("cart", JSON.stringify(cart));
     }
+    sessionStorage.setItem("cart", JSON.stringify(cart));
+    updateCartTotal();
   });
 });
