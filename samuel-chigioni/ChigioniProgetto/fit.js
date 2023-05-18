c3672c2c63bba753935999221fecfeb59a8fcd4e
Your Name <you@example.com>
2023-05-18 11:49:11 +0200

    progetto

--- samuel-chigioni/ChigioniProgetto/fit.js
+++ samuel-chigioni/ChigioniProgetto/fit.js
@@ -1,16 +1,26 @@
 
+function registraCodice() {
+  const promoInput = document.getElementById('promoInput');
+  const codicePromo = promoInput.value;
+  return codicePromo;
+}
+ 
+
 class Prodotto {
 
-  constructor(id,nome, prezzo) {
+  constructor(id,nome, prezzo, codicePromo, elimina) {
 
     this.id = id;
     this.nome = nome;
     this.prezzo = prezzo;
-    
+    this.codicePromo = codicePromo;
+    this.elimina = elimina; 
 
   }
 
 }
+
+
 var counter=1;
 
 function creaProdotto(element) {
@@ -19,102 +29,116 @@ function creaProdotto(element) {
   
   var prodotto;
 
+  
+
   if (selezione == 'Straps') {
 
-    prodotto = new Prodotto(counter,"Straps", 9.99);
+    prodotto = new Prodotto(counter,"Straps", 9.99,"Non inserito");
 
       }else if(selezione == 'Creatina'){
-        prodotto = new Prodotto(counter,"Creatina",19.99);
+        prodotto = new Prodotto(counter,"Creatina",19.99,"Non inserito");
 
       }else if(selezione == 'Proteine'){
-        prodotto = new Prodotto(counter,"Proteine",24.99);
+        prodotto = new Prodotto(counter,"Proteine",24.99,"Non inserito");
 
       }else if(selezione == 'T-shirt'){
-        prodotto = new Prodotto(counter,"T-shirt",19.99);
+        prodotto = new Prodotto(counter,"T-shirt",19.99,"Non inserito");
 
       }else if(selezione == 'Short'){
-        prodotto = new Prodotto(counter,"Short",29.99);
+        prodotto = new Prodotto(counter,"Short",29.99,"Non inserito");
 
       }else if(selezione == 'Pre-workout'){
-        prodotto = new Prodotto(counter,"Pre-workout",29.99);
+        prodotto = new Prodotto(counter,"Pre-workout",29.99,"Non inserito");
 
       }else if(selezione == 'Fit-shoes'){
-        prodotto = new Prodotto(counter,"Fit-shoes",49.99);
+        prodotto = new Prodotto(counter,"Fit-shoes",49.99,"Non inserito");
 
       }else if(selezione == 'Amminoacidi'){
-        prodotto = new Prodotto(counter,"Creatina",12.95);
+        prodotto = new Prodotto(counter,"Creatina",12.95,"Non inserito");
 
       }else if(selezione == 'Ammino'){
-        prodotto = new Prodotto(counter,"Creatina",15.99);
+        prodotto = new Prodotto(counter,"Creatina",15.99,"Non inserito");
         
   }
 
   console.log(prodotto)
 
-
-
 counter++;
-aggiungiAlCarrello(prodotto);
+ aggiungiAlCarrello(prodotto);
 
-}
-
-function aggiungiAlCarrello(prodotto){
-
-const newElementTr = document.createElement('tr');
-const newElementTh = document.createElement('th');
-
-const textId = document.createTextNode(prodotto.id);
-
-const newElementTdName = document.createElement('td');
-const textName = document.createTextNode(prodotto.nome);
-
-const newElementTdPrezzo = document.createElement('td');
-const textPrezzo = document.createTextNode(prodotto.prezzo);
-
-newElementTr.appendChild(newElementTh).appendChild(textId);
-newElementTr.appendChild(newElementTdName).appendChild(textName);
-newElementTr.appendChild(newElementTdPrezzo).appendChild(textPrezzo);
+ }
 
 
+function aggiungiAlCarrello(prodotto) {
+  const newElementTr = document.createElement('tr');
 
+  const newElementTh = document.createElement('th');
+  const textId = document.createTextNode(prodotto.id);
+  newElementTh.appendChild(textId);
 
-document.getElementById('corpo').appendChild(newElementTr);
+  const newElementTdName = document.createElement('td');
+  const textName = document.createTextNode(prodotto.nome);
+  newElementTdName.appendChild(textName);
 
+  const newElementTdPrezzo = document.createElement('td');
+  const textPrezzo = document.createTextNode(prodotto.prezzo);
+  newElementTdPrezzo.appendChild(textPrezzo);
 
+  const newElementTdPromo = document.createElement('td');
+  const textPromo = document.createTextNode(prodotto.codicePromo);
+  newElementTdPromo.appendChild(textPromo);
 
+  const newElementTdElimina = document.createElement('td');
 
-}
+  const deleteButton = document.createElement("button");
+  deleteButton.style = "color:white; border-color: white; background-color: white; box-shadow: 3px;";
 
-// CLICK SUL CARRELLO
-const button1 = document.getElementById("cart");
+  const deleteImage = document.createElement("img");
+  deleteImage.src = "immagini/delete.png";
+  deleteImage.style = "width:30px; height:30px";
 
-button1.addEventListener("click", function () {
-    window.scroll({
-        top: 1500,
-        left: 0,
-        behavior: "smooth"
-    });
-});
-//-----------------------------
+  deleteButton.appendChild(deleteImage);
+  deleteButton.onclick = function() {
+    deleteRow(this);
+  };
 
+  newElementTdElimina.appendChild(deleteButton);
 
+  newElementTr.appendChild(newElementTh);
+  newElementTr.appendChild(newElementTdName);
+  newElementTr.appendChild(newElementTdPrezzo);
+  newElementTr.appendChild(newElementTdPromo);
+  newElementTr.appendChild(newElementTdElimina);
 
-//SLIDER:-------------------------------------------------------
-
-
-let slideIndex = 1;
-showSlides(slideIndex);
-
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+  document.getElementById('corpo').appendChild(newElementTr);
 }
 
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+function deleteRow(btn) {
+  // Traverse up the DOM to find the closest <tr> element
+  var row = btn.closest("tr");
+  // Remove the row from the table
+  row.remove();
 }
 
+
+function scrollToSection() {
+  const dropdown = document.getElementById("productDropdown");
+  const selectedIndex = dropdown.selectedIndex;
+  
+  if (selectedIndex > 0) {
+    const sections = document.getElementsByTagName("section");
+    const section = sections[selectedIndex - 1];
+    section.scrollIntoView({ behavior: "smooth" });
+  }
+}
+
+
+
+//SLIDER:
+
+let slideIndex = 0;
+showSlides();
 
 function showSlides() {
   let i;
@@ -125,12 +149,10 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slide.length) { slideIndex = 1 }
   slide[slideIndex - 1].style.display = "block";
-  setTimeout(showSlides, 3000); // Cambia immagine ogni 2 secondi
+  setTimeout(showSlides, 2000); // Cambia immagine ogni 2 secondi
 }
 
 //-------------------------------------------------------------
-
-
 
 
 
